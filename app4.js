ab68f53ae78cb6f65db7caa27d59a1fa3b83cf52
Turhan A <55085934+Thoran98@users.noreply.github.com>
2023-03-30 15:55:25 +0300

    Add files via upload

---
+++ app4.js
@@ -0,0 +1,16 @@
+
+
+
+  const form= document.getElementById('form')
+  const search= document.getElementById('search')
+  const main= document.getElementById('main')
+
+
+  window.addEventListener('load', function() {
+    setTimeout(function() {
+      document.getElementById('loading').style.visibility = 'hidden';
+    }, 1500); // 1.5 saniye
+  });
+
+    
+   
